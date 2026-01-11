a319d9070bf1a9d5c1ccc8bfbf76532c3f9afa6b
mahls <holmesmatthew53@gmail.com>
2026-01-11 15:47:24 +1000

    Add Dependency Inversion Principle blog post

--- lib/posts/index.ts
+++ lib/posts/index.ts
@@ -2,9 +2,11 @@ import { strategyPatternPost } from './strategy-pattern-design-pattern';
 import { singletonPatternPost } from './singleton-design-pattern';
 import { repositoryPatternPost } from './repository-pattern';
 import { nvidiaSpectrumXPost } from './nvidia-spectrum-x-switches';
+import { dependencyInversionPost } from './dependency-inversion-principle';
 import { BlogPost } from './types';
 
 export const blogPosts: BlogPost[] = [
+  dependencyInversionPost,
   strategyPatternPost,
   singletonPatternPost,
   repositoryPatternPost,
